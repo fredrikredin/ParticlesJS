5554d276cc589fffaf6ac03b5214ee75d5171d3e
Fredrik Redin <fredrik.redin@gmail.com>
2020-01-27 19:46:59 +0100

    func. to add/remove particles on click

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 const particles = [];
-const particleCount = Math.floor(window.innerWidth / 50);
+const particleCount = Math.floor(window.innerWidth / 40);
+const clickParticleLimit = 20;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
 
     for (let i = 0; i < particleCount; i++)
-        particles.push(new Particle());
+        addParticle(random(width), random(height));
 
     strokeWeight(2);
     fill(255);
@@ -18,4 +19,31 @@ function draw() {
         p.draw();
         p.drawConnections(particles);
     });
+}
+
+function mousePressed(event) {
+    var x = event.x;
+    var y = event.y;
+    var removeCount = 0;
+
+    particles.forEach(p => {
+        if (dist(x, y, p.pos.x, p.pos.y) < clickParticleLimit) {
+            removeParticle(p);
+            removeCount++;
+        }
+    })
+
+    if (removeCount === 0)
+        addParticle(x,y);
+
+    return false; // prevent default
+}
+
+function addParticle(x, y) {
+    particles.push(new Particle(x, y));
+}
+
+function removeParticle(element) {
+    const index = particles.indexOf(element);
+    particles.splice(index, 1);
 }
--- particle.js
+++ particle.js
@@ -1,9 +1,9 @@
-let distlimit = 180;
-let alpha = 0;
+const drawConnectionLimit = 180;
 
 class Particle {
-    constructor() {
-        this.pos = createVector(random(width), random(height));
+    constructor(x,y)
+    {
+        this.pos = createVector(x,y);
         this.vel = createVector(random(-2, 2), random(-2, 2));
     }
 
@@ -17,9 +17,9 @@ class Particle {
         particles.forEach(p => {
             const distance = this.pos.dist(p.pos);
 
-            if (distance < distlimit) {
-                alpha = map(distance, 0, distlimit, 255, 0);
-                stroke(255, 255, 255, alpha);
+            if (distance < drawConnectionLimit) {
+                var strokeAlpha = map(distance, 0, drawConnectionLimit, 255, 0);
+                stroke(255, 255, 255, strokeAlpha);
                 line(this.pos.x, this.pos.y, p.pos.x, p.pos.y);
             }
         });
